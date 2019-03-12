9098035fd860e3381a69f49decb481790364b193
Ignacio Vizzo <ignaciovizzo@gmail.com>
2019-03-12 13:07:36 +0100

    Add copyright and guard

--- include/my_library.hpp
+++ include/my_library.hpp
@@ -1,5 +1,16 @@
+/**
+ * Created on 12-03-2019
+ *
+ * Copyright (c) 2019 Ignacio Vizzo
+ */
+
+#ifndef INCLUDE_MY_LIBRARY_HPP_
+#define INCLUDE_MY_LIBRARY_HPP_
+
 // This funtion performs a sum and that's it :P
 float my_sum(float x, float y);
 
 // This funtion performs a substract and that's it :P
 float my_substract(float x, float y);
+
+#endif  // INCLUDE_MY_LIBRARY_HPP_
