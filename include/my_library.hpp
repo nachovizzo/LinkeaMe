323b161db9b0303957ccc84e12a138d94a4114db
Ignacio Vizzo <ignaciovizzo@gmail.com>
2019-03-27 13:45:07 +0100

    Fix typo

--- include/my_library.hpp
+++ include/my_library.hpp
@@ -7,10 +7,10 @@
 #ifndef INCLUDE_MY_LIBRARY_HPP_
 #define INCLUDE_MY_LIBRARY_HPP_
 
-// This funtion performs a sum and that's it :P
+// This function performs a sum and that's it :P
 float my_sum(float x, float y);
 
-// This funtion performs a substract and that's it :P
+// This function performs a substract and that's it :P
 float my_substract(float x, float y);
 
 #endif  // INCLUDE_MY_LIBRARY_HPP_
