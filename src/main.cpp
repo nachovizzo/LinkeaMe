ddbe82eea5abfbd017b28a893ebe1e2d3d543a23
Ignacio Vizzo <ignaciovizzo@gmail.com>
2019-04-10 15:47:51 +0200

    Remove unused parameters

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <my_library/library_api.hpp>
 
-int main(int argc, char **argv) {
+int main() {
   float x = 1.0;
   float y = 2.0;
   std::cout << "The sum of " << x << " + " << y << " = " << my_sum(x, y) << "\n"
